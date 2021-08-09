79bb55fad968f5c9581ee4eade56196d736845cb
demonick82 <demonick82@gmail.com>
2021-08-10 01:05:58 +0300

    1.1. Состояние нити. [#229175] (Исправления)

--- src/main/java/concurrent/ThreadState.java
+++ src/main/java/concurrent/ThreadState.java
@@ -1,5 +1,6 @@
 package concurrent;
 
+
 public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(() -> {
@@ -8,19 +9,15 @@ public class ThreadState {
         Thread second = new Thread(() -> {
 
         });
-
         System.out.printf("%s %s%s", first.getName(), first.getState(), System.lineSeparator());
         System.out.printf("%s %s%s", second.getName(), second.getState(), System.lineSeparator());
         first.start();
         second.start();
 
-        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED ) {
             System.out.printf("%s %s%s", first.getName(), first.getState(), System.lineSeparator());
             System.out.printf("%s %s%s", second.getName(), second.getState(), System.lineSeparator());
-
-        }
-        if (first.getState() == Thread.State.TERMINATED && second.getState() == Thread.State.TERMINATED) {
-            System.out.println("Работа завершена");
-        }
+       }
+        System.out.println("Работа завершена");
     }
 }
