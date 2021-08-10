72f76b9018da0716ab8713734458fad2e2f832de
demonick82 <demonick82@gmail.com>
2021-08-10 22:31:01 +0300

    checkStyle

--- src/main/java/concurrent/Wget.java
+++ src/main/java/concurrent/Wget.java
@@ -2,12 +2,12 @@ package concurrent;
 
 public class Wget {
     public static void main(String[] args) {
-        Thread thread=new Thread(()->{
+        Thread thread = new Thread(() -> {
             try {
                 System.out.print("Start loading ... ");
-                for (int i = 0; i <=100 ; i++) {
+                for (int i = 0; i <= 100; i++) {
                     Thread.sleep(1000);
-                    System.out.printf("\rLoading : %d %%",i);
+                    System.out.printf("\rLoading : %d %%", i);
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
