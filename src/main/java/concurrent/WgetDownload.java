425a48ea9704c79b419b7e143587d4e7533a40ef
demonick82 <demonick82@gmail.com>
2021-08-15 23:13:51 +0300

    4. Скачивание файла с ограничением. [#144271 #189552] (Исправления)

--- src/main/java/concurrent/WgetDownload.java
+++ src/main/java/concurrent/WgetDownload.java
@@ -8,26 +8,29 @@ import java.net.URL;
 public class WgetDownload implements Runnable {
     private final String url;
     private final int speed;
+    private final String pathDownload;
 
-    public WgetDownload(String url, int speed) {
+    public WgetDownload(String url, int speed, String pathDownload) {
         this.url = url;
         this.speed = speed;
+        this.pathDownload = pathDownload;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(pathDownload)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
+            long startTimeStamp = System.nanoTime();
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                long startTimeStamp = System.nanoTime();
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
                 long finishTimeStamp = System.nanoTime();
                 long deltaTime = finishTimeStamp - startTimeStamp;
                 if (deltaTime < speed) {
                     Thread.sleep(speed - deltaTime);
                 }
+                startTimeStamp = System.nanoTime();
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -40,7 +43,7 @@ public class WgetDownload implements Runnable {
         }
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new WgetDownload(url, speed));
+        Thread wget = new Thread(new WgetDownload(url, speed, "out.txt"));
         wget.start();
         wget.join();
     }
