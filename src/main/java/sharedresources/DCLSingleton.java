2f291e3dcd43d5cf77bfdd8c2a0c3528edec726c
demonick82 <demonick82@gmail.com>
2021-08-16 00:17:16 +0300

    2. Модель памяти Java [#267917]

---
+++ src/main/java/sharedresources/DCLSingleton.java
@@ -0,0 +1,19 @@
+package sharedresources;
+
+public class DCLSingleton {
+    private static volatile DCLSingleton inst;
+
+    public static DCLSingleton instOf() {
+        if (inst == null) {
+            synchronized (DCLSingleton.class) {
+                if (inst == null) {
+                    inst = new DCLSingleton();
+                }
+            }
+        }
+        return inst;
+    }
+
+    private DCLSingleton() {
+    }
+}
