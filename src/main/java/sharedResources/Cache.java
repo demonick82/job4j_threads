7d1d697305c6b14e8964f637ef59fa3987280bdf
demonick82 <demonick82@gmail.com>
2021-08-15 23:58:32 +0300

    1. Синхронизация общих ресурсов. [#1096]

---
+++ src/main/java/sharedResources/Cache.java
@@ -0,0 +1,12 @@
+package sharedResources;
+
+public class Cache {
+    private static Cache cache;
+
+    public static synchronized Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}
