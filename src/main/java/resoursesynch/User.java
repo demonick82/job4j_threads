6b880d4abea31b2ed95129391b034e241716ba27
demonick82 <demonick82@gmail.com>
2021-08-23 00:04:43 +0300

    3. Класс хранилища пользователей UserStorage [#1104]

---
+++ src/main/java/resoursesynch/User.java
@@ -0,0 +1,31 @@
+package resoursesynch;
+
+public class User {
+    private int id;
+    private int amount;
+
+    public User(int id, int amount) {
+        this.id = id;
+        this.amount = amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "id=" + id
+                + ", amount=" + amount
+                + '}';
+    }
+}
---
+++ src/main/java/resoursesynch/UserStorage.java
@@ -0,0 +1,53 @@
+package resoursesynch;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ThreadSafe
+public class UserStorage {
+
+    @GuardedBy("this")
+    private Map<Integer, User> store = new HashMap<>();
+
+    public synchronized boolean add(User user) {
+        boolean rsl = false;
+        if (!store.containsKey(user.getId())) {
+            store.put(user.getId(), user);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    public synchronized boolean update(User user) {
+        boolean rsl = false;
+        if (store.containsKey(user.getId())) {
+            store.replace(user.getId(), user);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    public synchronized boolean delete(User user) {
+        boolean rsl = false;
+        if (store.containsKey(user.getId())) {
+            store.remove(user.getId());
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    public synchronized boolean transfer(int fromId, int told, int amount) {
+        boolean rsl = false;
+        User fromAccount = store.get(fromId);
+        User toAccount = store.get(told);
+        if (fromAccount != null || toAccount != null || fromAccount.getAmount() > toAccount.getAmount()) {
+            fromAccount.setAmount(fromAccount.getAmount() - amount);
+            toAccount.setAmount(toAccount.getAmount() + amount);
+            rsl = true;
+        }
+        return rsl;
+    }
+}
