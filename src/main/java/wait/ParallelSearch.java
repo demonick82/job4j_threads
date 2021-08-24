d1d62ee705876802a260ee8890f47d3e83420e98
demonick82 <demonick82@gmail.com>
2021-08-25 00:31:06 +0300

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/wait/ParallelSearch.java
@@ -0,0 +1,36 @@
+package wait;
+
+public class ParallelSearch {
+    public static void main(String[] args)  {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                });
+        producer.start();
+    }
+}
