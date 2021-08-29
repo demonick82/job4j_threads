42224597cbde3f4f65908ed79ae9201633edd0c8
demonick82 <demonick82@gmail.com>
2021-08-30 01:03:02 +0300

    1. Неблокирующий кеш [#4741]

--- src/main/java/cas/Base.java
+++ src/main/java/cas/Base.java
@@ -30,12 +30,16 @@ public class Base {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Base base = (Base) o;
-        return id == base.id &&
-                version == base.version &&
-                Objects.equals(name, base.name);
+        return id == base.id
+                && version == base.version
+                && Objects.equals(name, base.name);
     }
 
     @Override
