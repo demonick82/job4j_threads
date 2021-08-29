5187a9378818a19f0f77aa060c8b25573fd5a22f
demonick82 <demonick82@gmail.com>
2021-08-29 22:37:41 +0300

    0. CAS - операции [#6859]

---
+++ src/main/java/cas/CASCount.java
@@ -0,0 +1,23 @@
+package cas;
+
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class CASCount {
+    private final AtomicReference<Integer> count = new AtomicReference<>();
+
+    public void increment() {
+        int currCount;
+        int nextCount;
+        do {
+            currCount = count.get();
+            nextCount = currCount + 1;
+        } while (!count.compareAndSet(currCount, nextCount));
+    }
+
+    public int get() {
+        return count.get();
+    }
+}
