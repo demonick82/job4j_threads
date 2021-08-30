3c2f19e12a7be9bc14e973e8bba95730410b72db
demonick82 <demonick82@gmail.com>
2021-08-31 00:24:21 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/pool/EmailNotification.java
@@ -0,0 +1,31 @@
+package pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public void emailTo(User user) {
+        pool.submit(() -> {
+            String subject = String.format("Notification %s to email %s", user.getUserName(), user.getEmail());
+            String body = String.format("Add a new event to %s", user.getEmail());
+            send(subject, body, user.getEmail());
+        });
+    }
+
+    public void send(String subject, String body, String email) {
+
+    }
+
+    public void close() {
+        pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
